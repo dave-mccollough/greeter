92cfd25e4556cbbb02a445adee95b1147df213f9
Dave McCollough <davemccollough@gmail.com>
2025-08-08 19:40:33 -0600

    Rust greeter app

---
+++ greeter/target/debug/deps/greeter-a8610b1983ca05fd.d
@@ -0,0 +1,5 @@
+/Users/davemccollough/Source/rust_projects/greeter/greeter/target/debug/deps/greeter-a8610b1983ca05fd.d: src/main.rs
+
+/Users/davemccollough/Source/rust_projects/greeter/greeter/target/debug/deps/greeter-a8610b1983ca05fd: src/main.rs
+
+src/main.rs:
---
+++ greeter/target/debug/greeter.d
@@ -0,0 +1,1 @@
+/Users/davemccollough/Source/rust_projects/greeter/greeter/target/debug/greeter: /Users/davemccollough/Source/rust_projects/greeter/greeter/src/main.rs
